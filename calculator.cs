391a38d51530ef449cbfecb357841560fcb69c56
forid7 <61509809+forid7@users.noreply.github.com>
2020-12-28 12:12:14 +0600

    Add files via upload

---
+++ calculator.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        double fstNum, secNum;
+        string oper;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(txtDisplay.Text.Length>0)
+            {
+                txtDisplay.Text = txtDisplay.Text.Remove(txtDisplay.Text.Length - 1, 1);
+            }
+
+            if (txtDisplay.Text== "")
+            {
+                txtDisplay.Text = "0";
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "0";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if(txtDisplay.Text=="0")
+            {
+                txtDisplay.Text = "7";
+            }
+            else
+            {
+                txtDisplay.Text = txtDisplay.Text + "7";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "0";
+            string f, s;
+            f = Convert.ToString(fstNum);
+            s = Convert.ToString(secNum);
+
+            f = "";
+            s = "";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == "0")
+            {
+                txtDisplay.Text = "8";
+            }
+            else
+            {
+                txtDisplay.Text = txtDisplay.Text + "8";
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == "0")
+            {
+                txtDisplay.Text = "9";
+            }
+            else
+            {
+                txtDisplay.Text = txtDisplay.Text + "9";
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == "0")
+            {
+                txtDisplay.Text = "4";
+            }
+            else
+            {
+                txtDisplay.Text = txtDisplay.Text + "4";
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == "0")
+            {
+                txtDisplay.Text = "5";
+            }
+            else
+            {
+                txtDisplay.Text = txtDisplay.Text + "5";
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == "0")
+            {
+                txtDisplay.Text = "6";
+            }
+            else
+            {
+                txtDisplay.Text = txtDisplay.Text + "6";
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == "0")
+            {
+                txtDisplay.Text = "1";
+            }
+            else
+            {
+                txtDisplay.Text = txtDisplay.Text + "1";
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == "0")
+            {
+                txtDisplay.Text = "2";
+            }
+            else
+            {
+                txtDisplay.Text = txtDisplay.Text + "2";
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == "0")
+            {
+                txtDisplay.Text = "3";
+            }
+            else
+            {
+                txtDisplay.Text = txtDisplay.Text + "3";
+            }
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == "0")
+            {
+                txtDisplay.Text = "0";
+            }
+            else
+            {
+                txtDisplay.Text = txtDisplay.Text + "0";
+            }
+        }
+
+        private void btnDec_Click(object sender, EventArgs e)
+        {
+            if (btnDec.Text == ".")
+            {
+                if (!txtDisplay.Text.Contains("."))
+                {
+                    txtDisplay.Text = txtDisplay.Text + btnDec.Text;
+                }
+
+            }
+        }
+
+        private void btnEqu_Click(object sender, EventArgs e)
+        {
+            secNum = double.Parse(txtDisplay.Text);
+
+            switch(oper)
+            {
+                case "+":
+                    txtDisplay.Text = (fstNum + secNum).ToString();
+                    break;
+                case "-":
+                    txtDisplay.Text = (fstNum - secNum).ToString();
+                    break;
+                case "*":
+                    txtDisplay.Text = (fstNum * secNum).ToString();
+                    break;
+                case "/":
+                    txtDisplay.Text = (fstNum / secNum).ToString();
+                    break;
+
+                default:
+                    break;
+
+
+            }
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            fstNum = double.Parse(txtDisplay.Text);
+            oper = "-";
+            txtDisplay.Text = "";
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            fstNum = double.Parse(txtDisplay.Text);
+            oper = "*";
+            txtDisplay.Text = "";
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            fstNum = double.Parse(txtDisplay.Text);
+            oper = "/";
+            txtDisplay.Text = "";
+        }
+
+        private void btnPM_Click(object sender, EventArgs e)
+        {
+            double q = Convert.ToDouble(txtDisplay.Text);
+            txtDisplay.Text = Convert.ToString(-1 * q);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            fstNum = double.Parse(txtDisplay.Text);
+            oper = "+";
+            txtDisplay.Text = "";
+        }
+    }
+}
